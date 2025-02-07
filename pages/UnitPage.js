090712ac66fc237ff5bf5dc5f26440fc94b9b524
Jhkomal <komaljha5567@gmail.com>
2025-02-07 14:29:27 -0800

    feat:create unitpage to handle test related to unit

---
+++ pages/UnitPage.js
@@ -0,0 +1,32 @@
+import BasePage from '../BasePage';
+
+class UnitPage extends BasePage {
+    constructor(page) {
+        super(page);
+        this.expandItemsButton = page.getByRole('button', { name: 'Expand "Items"' });
+        this.unitsLink = page.getByRole('listitem').filter({ hasText: 'Units' });
+        this.newUnitLink = page.getByRole('link', { name: 'New Unit' });
+
+        this.nameField = page.getByRole('textbox', { name: 'Name *' });
+        this.shortNameField = page.getByRole('textbox', { name: 'Short Name' });
+
+        this.createButton = page.getByRole('button', { name: 'Create' });
+    }
+
+    async navigateToNewUnit() {
+        await this.expandItemsButton.click();
+        await this.unitsLink.click();
+        await this.newUnitLink.click();
+    }
+
+    async fillUnitDetails(name, shortName) {
+        await this.nameField.fill(name);
+        await this.shortNameField.fill(shortName);
+    }
+
+    async createUnit() {
+        await this.createButton.click();
+    }
+}
+
+export default UnitPage;
