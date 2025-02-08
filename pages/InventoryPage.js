3fb050516d810771100dec83ceca40bdd9cd717d
Jhkomal <komaljha5567@gmail.com>
2025-02-07 17:22:01 -0800

    feat:add inventorypage to search item

---
+++ pages/InventoryPage.js
@@ -0,0 +1,30 @@
+class InventoryPage {
+    constructor(page) {
+        this.page = page;
+
+        // Locators
+        this.expandItemsButton = page.getByRole('button', { name: 'Expand "Items"' });
+        this.inventoryLedgerLink = page.getByText('inventoryInventory Ledger');
+        this.searchInput = page.getByPlaceholder('Search');
+        this.searchResult = page.getByRole('cell', { name: 'locker' });
+    }
+
+    // Actions
+    async expandItems() {
+        await this.expandItemsButton.click();
+    }
+
+    async openInventoryLedger() {
+        await this.inventoryLedgerLink.click();
+    }
+
+    async searchItem(itemName) {
+        await this.searchInput.fill(itemName);
+    }
+
+    async selectSearchResult() {
+        await this.searchResult.click();
+    }
+}
+
+export default InventoryPage;
